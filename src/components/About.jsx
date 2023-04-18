1c7dc4b22a8cdfa38b0016ca3ce2131473148774
aretaleks <31519197+aretaleks@users.noreply.github.com>
2023-04-18 13:39:06 +0300

    text update text update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <h2>My name is Benjamin Lin and I am a React Developer.</h2>;
+  return <h2>My name is Aleksander Aret and I am a React Developer.</h2>;
 };
 
 export default About;
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,22 +3,25 @@ import React from "react";
 const Resume = () => {
   return (
     <div>
-      <h1>Benjamin Lin</h1>
+      <h1>Aleksander Aret</h1>
       <h2>Education</h2>
-      <h3>Cal Poly SLO</h3>
+      <h3>Taltech</h3>
       <ul>
-        <li>Bachelors of Science in Computer Engineering</li>
+        <li>Masters of Mechanical Engineering</li>
         <li>Class of 2016</li>
-        <li>GPA: 3.6</li>
+        <li>GPA: 4.2</li>
       </ul>
       <h2>Work History</h2>
-      <h3>Microsoft - Content Developer</h3>
+      <h3>Unifiedpost Group - Fintech Developer</h3>
       <ul>
-        <li>Wrote online courses in React.js</li>
-        <li>Hadooped the Big Data using Machine Learning with Saas on Azure</li>
+        <li>Developing invoicing solutions for small and large businesses</li>
       </ul>
       <h2>Skills</h2>
       <ul>
+        <li>SQL</li>
+        <li>Java</li>
+        <li>Groovy</li>
+        <li>XSL, XSL-FO, Apache FOP</li>
         <li>React.js</li>
         <li>React Router</li>
         <li>Node.js</li>
