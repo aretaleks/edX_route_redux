5229fd6032bc69136243f6038807bb241c51a25a
aretaleks <31519197+aretaleks@users.noreply.github.com>
2023-04-18 13:10:26 +0300

    react and react-dom update react and react-dom update to 16.8.1 to use hooks.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,40 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Prompt } from "react-router-dom";
 
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-  }
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-  handleSubmit() {
-    this.setState({ value: "" });
-  }
-  render() {
-    var style = {
-      width: 300,
-      height: 60,
-      margin: 10
-    };
-    return (
+function Contact() {
+  const [value, setValue] = useState("");
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  const handleSubmit = () => {
+    setValue("");
+  };
+  var style = {
+    width: 300,
+    height: 60,
+    margin: 10
+  };
+  return (
+    <div>
+      <input value={value} onChange={handleChange} style={style} />
       <div>
-        <input
-          value={this.state.value}
-          onChange={this.handleChange.bind(this)}
-          style={style}
-        />
-        <div>
-          <button onClick={this.handleSubmit.bind(this)}>Send</button>
-        </div>
-        <Prompt
-          when={this.state.value !== ""}
-          message="are you sure you want to leave without sending a message?"
-        />
+        <button onClick={handleSubmit}>Send</button>
       </div>
-    );
-  }
+      <Prompt
+        when={value !== ""}
+        message="are you sure you want to leave without sending a message?"
+      />
+    </div>
+  );
 }
 
-module.exports = Contact;
+export default Contact;
